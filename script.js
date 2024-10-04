fdf46da1d410a3451f62a85c4b81f7d0ff54cbed
Atulp24 <apk200209@gmail.com>
2024-10-04 16:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+   const result = [];
+	let currentSum = 0;
+	let currentSubArray = [];
+
+	for(let num of arr) {
+		if(currentSum + num > n) {
+			result.push(currentSubArray);
+			currentSum = 0;
+			currentSubArray = [];
+		}
+		currentSubArray.push(num);
+		currentSum += num;
+	}
+	if(currentSubArray.length > 0) {
+		result.push(currentSubArray);
+	}
+	return result;
 };
 
 const n = prompt("Enter n: ");
